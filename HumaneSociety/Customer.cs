5039b6878f99dc5e1f13b7495b7f07ca3ff47e26
Ljiljana <dstoj1@yahoo.comgit config --global user.email dstoj1@yahoo.com>
2017-09-05 15:46:47 -0500

    Created EmployeeUser and Cusotmer Classe. Moved GetTypeOfAnimal from program to Customer,

--- HumaneSociety/Customer.cs
+++ HumaneSociety/Customer.cs
@@ -8,5 +8,31 @@ namespace HumaneSociety
 {
     class Customer
     {
+        Animal animals = new Animal();
+
+        public Customer()
+        {
+
+        }
+    }
+    public void GetTypeOfAnimal()
+    {
+        Console.WriteLine("What type of Animal are you searching for?");
+        animals = Console.ReadLine().ToLower();
+        switch (animals)
+        {
+            case "Dog":
+                Console.WriteLine("You choose a dog");
+                break;
+            case "Cat":
+                Console.WriteLine("You choose a cat");
+                break;
+            case "Small Animals":
+                Console.WriteLine("You choose a small animal");
+                break;
+            default:
+                break;
+
+        }
     }
 }
--- HumaneSociety/Program.cs
+++ HumaneSociety/Program.cs
@@ -36,7 +36,6 @@ namespace HumaneSociety
             Console.WriteLine("");
 
 
-
             Adopter person = new Adopter();
 
             Console.WriteLine("Please establish and User Id");
@@ -111,6 +110,58 @@ namespace HumaneSociety
                     break;
                                   
             }
+             public static List<Animal> GetAnimal()
+        {
+            HumaneSocietyDataContext Data = new HumaneSocietyDataContext();
+            var animal = from entries in Data.Animals select entries;
+            return animal.ToList();
+        }
+        public Animal GetSelectedAnimal(int Animal_ID)
+        {
+            HumaneSocietyDataContext Data = new HumaneSocietyDataContext();
+            Animal typeOfAnimal = (from entries in Data.Animals where entries.Animal_ID == Animal_ID select entries).First();
+            return typeOfAnimal;
+        }
+        public static bool AddAnimal(Animal animal)
+        {
+            try
+            {
+                HumaneSocietyDataContext Data = new HumaneSocietyDataContext();
+                //Console.WriteLine(things.ToList()[0].Name + " " + things.ToList()[0].Animal_ID);
+                Data.Animals.InsertOnSubmit(animal);
+                Data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static bool DeleteAnimal(int Animal_ID)
+        {
+            try
+            {
+                HumaneSocietyDataContext Data = new HumaneSocietyDataContext;
+                Animal obj = (from entries in Data.Animals where entries.Animal_ID.Contains(Animal_ID) select entries).First();
+                if (obj != null)
+                {
+
+                    Data.Animals.DeleteOnSubmit(obj);
+                    Data.SubmitChanges();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
+
 
 
         }
