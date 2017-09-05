8a6cfbd60aaefdab2de7f81835e7fef01cc1aaa8
Ljiljana <dstoj1@yahoo.comgit config --global user.email dstoj1@yahoo.com>
2017-09-05 14:07:41 -0500

    Added AddAnimal method

--- HumaneSociety/Animal.cs
+++ HumaneSociety/Animal.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace HumaneSociety
 {
-    class Animal
+    public class Animal
     {
+        public static List<Animal>GetAnimal()
+        {
+                HumaneSocietyDataContext Data = new HumaneSocietyDataContext();
+                var doggy = from entries in Data.Animals select entries;
+                return doggy.ToList();
+        }
+        public static bool AddAnimal()
+        {
+            try
+            {
+                HumaneSocietyDataContext Data = new HumaneSocietyDataContext();
+                //Console.WriteLine(things.ToList()[0].Name + " " + things.ToList()[0].Animal_ID);
+                Data.Animals.InsertOnSubmit(doggy);
+                Data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
     }
 }
