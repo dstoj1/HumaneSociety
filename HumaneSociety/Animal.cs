457d5bb08ce14c1151a441867eb4b52d9b7c6600
Ljiljana <dstoj1@yahoo.comgit config --global user.email dstoj1@yahoo.com>
2017-09-05 14:33:37 -0500

    Added GetSelectedAnimal method

--- HumaneSociety/Animal.cs
+++ HumaneSociety/Animal.cs
@@ -11,22 +11,36 @@ namespace HumaneSociety
         public static List<Animal>GetAnimal()
         {
                 HumaneSocietyDataContext Data = new HumaneSocietyDataContext();
-                var doggy = from entries in Data.Animals select entries;
-                return doggy.ToList();
+                var animal = from entries in Data.Animals select entries;
+                return animal.ToList();
         }
-        public static bool AddAnimal()
+        public Animal GetSelectedAnimal(int Animal_ID)
+        {
+                 HumaneSocietyDataContext Data = new HumaneSocietyDataContext();
+            Animal typeOfAnimal = (from entries in Data.Animals where entries.Animal_ID == Animal_ID select entries).First();
+            return typeOfAnimal;
+        }
+        public static bool AddAnimal(Animal animal)
         {
             try
             {
                 HumaneSocietyDataContext Data = new HumaneSocietyDataContext();
                 //Console.WriteLine(things.ToList()[0].Name + " " + things.ToList()[0].Animal_ID);
-                Data.Animals.InsertOnSubmit(doggy);
+                Data.Animals.InsertOnSubmit(animal);
                 Data.SubmitChanges();
                 return true;
             }
             catch
             {
                 return false;
+            }
+        }
+        public static bool DeleteAnimal(int Animal_ID)
+        {
+            try
+            {
+                HumaneSocietyDataContext Data = new HumaneSocietyDataContext;
+
             }
         }
 
